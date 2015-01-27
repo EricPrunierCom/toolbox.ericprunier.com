741005202fb6bc8ed8b143a6c99704068c46c4d9
Eric Prunier <eric.prunier@gmail.com>
2015-01-27 19:00:54 +0100

    Add 3 request headers + display response headers

--- main.js
+++ main.js
@@ -1,40 +1,67 @@
 'use strict';
 
 $(document).ready(function() {
-    $('#test-url').on('click', testURL);
-    $('#close-response').on('click', closeResponse);
-    $('#close-error').on('click', closeError);
+  $('#test-url').on('click', testURL);
+  $('#close-response').on('click', closeResponse);
+  $('#close-error').on('click', closeError);
+  $('.add-header').on('click', addHeader);
+  $('.remove-header').on('click', removeHeader);
 });
 
 function testURL() {
-    $.ajax({
-        url: $('#url').val()
-    }).done(function (data, textStatus, jqXHR) {
-        $('#errors').hide();
+  var headers = _($('.header')).map(function (header) {
+    var name = $(header).find('.header-name').val();
+    var value = $(header).find('.header-value').val();
+    return {
+      name: name,
+      value: value
+    };
+  }).filter(function (header) {
+    return header.name
+  });
 
-        var response = '';
-        var contentType = jqXHR.getResponseHeader('Content-Type');
-        if (contentType.indexOf('application/json') > -1) {
-            response = JSON.stringify(data);
-        } else {
-            response = data;
-        }
-        $('#response-content').text(response);
-        $('#response').show();
-    }).fail(function (jqXHR, textStatus, errorThrown) {
-        $('#response').hide();
+  $.ajax({
+    url: $('#url').val(),
+    beforeSend: function (jqXHR) {
+      _(headers).forEach(function (header) {
+        jqXHR.setRequestHeader(header.name, header.value);
+      });
+    }
+  }).done(function (data, textStatus, jqXHR) {
+    $('#errors').hide();
 
-        $('#error').append(errorThrown || 'Internal error');
-        $('#errors').show();
-    });
+    var response = '';
+    var contentType = jqXHR.getResponseHeader('Content-Type');
+    if (contentType.indexOf('application/json') > -1) {
+      response = JSON.stringify(data, undefined, 4);
+    } else {
+      response = data;
+    }
+    $('#response-headers').html('<pre>' + jqXHR.getAllResponseHeaders() + '</pre>');
+    $('#response-body').html('<pre>' + response + '</pre>');
+    $('#response').show();
+  }).fail(function (jqXHR, textStatus, errorThrown) {
+    $('#response').hide();
+
+    $('#error').append(errorThrown || 'Internal error');
+    $('#errors').show();
+  });
 }
 
 function closeResponse() {
-    $('#response-content').text('');
-    $('#response').hide();
+  $('#response-content').text('');
+  $('#response').hide();
 }
 
 function closeError() {
-    $('#error').text('');
-    $('#errors').hide();
+  $('#error').text('');
+  $('#errors').hide();
+}
+
+function addHeader() {
+
+}
+
+function removeHeader() {
+
 }
